39e3f14ba2886e1a6d65165746d94cd7a1fc43b1
Conrad Parker <conrad@metadecks.org>
2012-03-14 13:49:03 +0800

    Parse: new module

---
+++ HeapScope/Parse.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wall #-}
+
+module HeapScope.Parse (
+  -- * Functions
+  hpParse
+) where
+
+import Control.Applicative
+import Data.Attoparsec.ByteString.Char8
+import Data.ByteString (ByteString)
+import qualified Data.Map as Map
+
+import HeapScope.HeapProfile
+
+hpParse :: HeapProfile -> Parser (HeapProfile, [HeapProfile])
+hpParse hp = do
+    hp' <- parseJob hp <|> parseDate hp <|> parseSampleUnit hp <|> parseValueUnit hp <|> parseSample hp
+    return (hp', [hp'])
+
+labelledString :: ByteString -> Parser ByteString
+labelledString l = do
+    string l
+    skipSpace
+    char '\"'
+    t <- takeTill (== '\"')
+    char '\"'
+    endOfLine
+    return t
+
+labelledDouble :: ByteString -> Parser Double
+labelledDouble l = do
+    string l
+    skipSpace
+    d <- double
+    endOfLine
+    return d
+
+parseJob :: HeapProfile -> Parser HeapProfile
+parseJob hp = do
+    t <- labelledString "JOB"
+    let hdr = (hpHeader hp) { hpJob = t }
+    return hp{hpHeader = hdr}
+    
+parseDate :: HeapProfile -> Parser HeapProfile
+parseDate hp = do
+    t <- labelledString "DATE"
+    let hdr = (hpHeader hp) { hpDate = t }
+    return hp{hpHeader = hdr}
+
+parseSampleUnit :: HeapProfile -> Parser HeapProfile
+parseSampleUnit hp = do
+    t <- labelledString "SAMPLE_UNIT"
+    let hdr = (hpHeader hp) { hpSampleUnit = t }
+    return hp{hpHeader = hdr}
+
+parseValueUnit :: HeapProfile -> Parser HeapProfile
+parseValueUnit hp = do
+    t <- labelledString "VALUE_UNIT"
+    let hdr = (hpHeader hp) { hpValueUnit = t }
+    return hp{hpHeader = hdr}
+
+parseSample :: HeapProfile -> Parser HeapProfile
+parseSample hp = do
+    hp' <- parseSampleStart (clearHeapProfile hp)
+    hp'' <- parseSampleValues hp'
+    parseSampleEnd hp''
+
+parseSampleStart :: HeapProfile -> Parser HeapProfile
+parseSampleStart hp = do
+    d <- labelledDouble "BEGIN_SAMPLE"
+    return hp{hpSampleStart = Just d}
+
+parseSampleEnd :: HeapProfile -> Parser HeapProfile
+parseSampleEnd hp = do
+    d <- labelledDouble "END_SAMPLE"
+    return hp{hpSampleEnd = Just d}
+
+parseSampleValues :: HeapProfile -> Parser HeapProfile
+parseSampleValues hp = do
+    vs <- many parseSampleValue
+    return hp { hpSamples = Map.fromList vs }
+
+parseSampleValue :: Parser (ByteString, Int)
+parseSampleValue = do
+    n <- takeTill isSpace
+    skipSpace
+    d <- decimal
+    endOfLine
+    return (n, d)
+
--- src/heapscope.hs
+++ src/heapscope.hs
@@ -26,6 +26,7 @@ import Data.ZoomCache.Codec
 import System.Environment (getArgs)
 
 import HeapScope.HeapProfile
+import HeapScope.Parse
 
 ----------------------------------------------------------------------
 
@@ -240,80 +241,4 @@ hpEnum = I.unfoldConvStream hpIter
 
 hpIter :: MonadIO m => HeapProfile -> Iteratee ByteString m (HeapProfile, [HeapProfile])
 hpIter = parserToIteratee . hpParse
-
-hpParse :: HeapProfile -> Parser (HeapProfile, [HeapProfile])
-hpParse hp = do
-    hp' <- parseJob hp <|> parseDate hp <|> parseSampleUnit hp <|> parseValueUnit hp <|> parseSample hp
-    return (hp', [hp'])
-
-labelledString :: ByteString -> Parser ByteString
-labelledString l = do
-    string l
-    skipSpace
-    char '\"'
-    t <- takeTill (== '\"')
-    char '\"'
-    endOfLine
-    return t
-
-labelledDouble :: ByteString -> Parser Double
-labelledDouble l = do
-    string l
-    skipSpace
-    d <- double
-    endOfLine
-    return d
-
-parseJob :: HeapProfile -> Parser HeapProfile
-parseJob hp = do
-    t <- labelledString "JOB"
-    let hdr = (hpHeader hp) { hpJob = t }
-    return hp{hpHeader = hdr}
-    
-parseDate :: HeapProfile -> Parser HeapProfile
-parseDate hp = do
-    t <- labelledString "DATE"
-    let hdr = (hpHeader hp) { hpDate = t }
-    return hp{hpHeader = hdr}
-
-parseSampleUnit :: HeapProfile -> Parser HeapProfile
-parseSampleUnit hp = do
-    t <- labelledString "SAMPLE_UNIT"
-    let hdr = (hpHeader hp) { hpSampleUnit = t }
-    return hp{hpHeader = hdr}
-
-parseValueUnit :: HeapProfile -> Parser HeapProfile
-parseValueUnit hp = do
-    t <- labelledString "VALUE_UNIT"
-    let hdr = (hpHeader hp) { hpValueUnit = t }
-    return hp{hpHeader = hdr}
-
-parseSample :: HeapProfile -> Parser HeapProfile
-parseSample hp = do
-    hp' <- parseSampleStart (clearHeapProfile hp)
-    hp'' <- parseSampleValues hp'
-    parseSampleEnd hp''
-
-parseSampleStart :: HeapProfile -> Parser HeapProfile
-parseSampleStart hp = do
-    d <- labelledDouble "BEGIN_SAMPLE"
-    return hp{hpSampleStart = Just d}
-
-parseSampleEnd :: HeapProfile -> Parser HeapProfile
-parseSampleEnd hp = do
-    d <- labelledDouble "END_SAMPLE"
-    return hp{hpSampleEnd = Just d}
-
-parseSampleValues :: HeapProfile -> Parser HeapProfile
-parseSampleValues hp = do
-    vs <- many parseSampleValue
-    return hp { hpSamples = Map.fromList vs }
-
-parseSampleValue :: Parser (ByteString, Int)
-parseSampleValue = do
-    n <- takeTill isSpace
-    skipSpace
-    d <- decimal
-    endOfLine
-    return (n, d)
 
